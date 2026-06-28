759d5007b6da05add8e16d8d5c32bd63b0b8fb96
Hoooooou-Jun <jipkim2@gmail.com>
2026-06-29 00:26:17 +0900

    [Fix] 토큰 등록 시 구버전 호환성 추가

--- src/modules/push-token/presentation/dto/request/push-token-request.dto.ts
+++ src/modules/push-token/presentation/dto/request/push-token-request.dto.ts
@@ -1,14 +1,18 @@
 import { ApiProperty } from '@nestjs/swagger';
-import { IsString } from 'class-validator';
+import { IsOptional, IsString } from 'class-validator';
 
 export class RegisterPushTokenDto {
   @ApiProperty({ example: 'ExponentPushToken[xxxxxx]', required: true })
   @IsString()
-  token: string;
+  token?: string;
+
+  @ApiProperty({ example: 1, required: false })
+  @IsOptional()
+  userId?: number;
 }
 
 export class DeletePushTokenDto {
   @ApiProperty({ example: 'ExponentPushToken[xxxxxx]' })
   @IsString()
-  token: string;
+  token?: string;
 }
